3e39735aa3a5dd99d28a9ee89847b7b2b8ffe20f
bit-berger <147028909+bit-berger@users.noreply.github.com>
2025-07-19 16:25:34 +0000

    Add files via upload

---
+++ scripts/update_snowflake.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+# updateing the proxy by compiling newer versions
+
+# update system
+apt update
+sudo apt upgrade -y
+sudo apt autoremove -y
+sudo apt autoclean -y
+
+sudo systemctl stop snowflake.service
+
+cd /home/root/
+git pull https://gitlab.torproject.org/tpo/anti-censorship/pluggable-transports/snowflake.git
+go build -C snowflake/proxy
+sudo rm /opt/proxy
+sudo mv snowflake/proxy/proxy /opt
+
+sudo systemctl enable snowflake.service
+sudo systemctl start snowflake.service
