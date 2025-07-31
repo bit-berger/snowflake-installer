e422b69d63ff568f679da4c30477fdda01416aad
bit-berger <147028909+bit-berger@users.noreply.github.com>
2025-07-31 13:42:21 +0000

    Add files via upload

--- scripts/update_snowflake.sh
+++ scripts/update_snowflake.sh
@@ -2,12 +2,6 @@
 #update the proxy by compiling newer versions
 #https://github.com/bit-berger/snowflake-installer
 
-#update system
-apt update
-sudo apt upgrade -y
-sudo apt autoremove -y
-sudo apt autoclean -y
-
 #stop systemd service
 sudo systemctl stop snowflake.service
 
